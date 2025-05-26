6c756c8c6205b2a090a0ca0e50a2b5557a504129
Aleev141 <roman.galeev.2016@mail.ru>
2025-05-26 15:59:28 +0500

    update 2.6

--- src-en/data/trc_scripts/trc_weaponFunctions.lua
+++ src-en/data/trc_scripts/trc_weaponFunctions.lua
@@ -16,8 +16,8 @@ local burstSpriteFixes = mods.trc.burstSpriteFixes
 burstSpriteFixes["TRC_CASH_GUN"] = true
 
 script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE, function(projectile, weapon)
-    local fixBurstSprite = nil
-    if pcall(function() fixBurstSprite = burstSpriteFixes[weapon.blueprint.name] end) and fixBurstSprite then
+    local isFake = projectile.death_animation.fScale == 0.25 
+    if burstSpriteFixes[weapon and weapon.blueprint and weapon.blueprint.name] and not isFake then
         projectile:Initialize(Hyperspace.Blueprints:GetWeaponBlueprint(projectile.extend.name))
     end
 end)
@@ -30,8 +30,8 @@ burstMultiBarrel["TRC_CASH_GUN"] = {
 }
 
 script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE, function(projectile, weapon)
-    local burstBarrelData = nil
-    if pcall(function() burstBarrelData = burstMultiBarrel[weapon.blueprint.name] end) and burstBarrelData then
+    local burstBarrelData = burstMultiBarrel[weapon and weapon.blueprint and weapon.blueprint.name]
+    if burstBarrelData then
         local offset = (burstBarrelData.barrelCount - weapon.queuedProjectiles:size()%burstBarrelData.barrelCount - 1)*burstBarrelData.barrelOffset
         if weapon.mount.mirror then offset = -offset end
         if weapon.mount.rotate then
@@ -49,8 +49,8 @@ statChargers["TRC_MAGNIFIER_2"] = {{stat = "iDamage"},{stat = "breachChance"},{s
 statChargers["TRC_MAGNIFIER_PIERCE"] = {{stat = "iShieldPiercing"},{stat = "breachChance"}}
 statChargers["TRC_MAGNIFIER_ION"] = {{stat = "iIonDamage"},{stat = "iStun"}}
 script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE, function(projectile, weapon)
-    local statBoosts = nil
-    if pcall(function() statBoosts = statChargers[weapon.blueprint.name] end) and statBoosts then
+    local statBoosts = statChargers[weapon and weapon.blueprint and weapon.blueprint.name]
+    if statBoosts then
         local boost = weapon.queuedProjectiles:size() -- Gets how many projectiles are charged up (doesn't include the one that was already shot)
         weapon.queuedProjectiles:clear() -- Delete all other projectiles
         for _, statBoost in ipairs(statBoosts) do -- Apply all stat boosts
@@ -71,12 +71,12 @@ cooldownChargers["TRC_MAGNIFIER_PIERCE"] = 1.5
 cooldownChargers["TRC_MAGNIFIER_ION"] = 1.2
 
 script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
-    local weapons = nil
-    if pcall(function() weapons = ship.weaponSystem.weapons end) and weapons then
+    local weapons = ship and ship.weaponSystem and ship.weaponSystem.weapons
+    if weapons then
         for weapon in vter(weapons) do
             if weapon.chargeLevel ~= 0 and weapon.chargeLevel < weapon.weaponVisual.iChargeLevels then
-                local cdBoost = nil
-                if pcall(function() cdBoost = cooldownChargers[weapon.blueprint.name] end) and cdBoost then
+                local cdBoost = cooldownChargers[weapon and weapon.blueprint and weapon.blueprint.name]
+                if cdBoost then
                     local cdLast = userdata_table(weapon, "mods.trc.weaponStuff").cdLast
                     if cdLast and weapon.cooldown.first > cdLast then
                         -- Calculate the new charge level from number of charges and charge level from last frame
@@ -100,4 +100,16 @@ script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
             end
         end
     end
+end)
+
+script.on_internal_event(Defines.InternalEvents.WEAPON_RENDERBOX, function(weapon, cooldown, maxCooldown, chargeString, damageString, shotLimitString)
+    local chargerBoost = cooldownChargers[weapon and weapon.blueprint and weapon.blueprint.name]
+    if chargerBoost then
+        local first, second = chargeString:match("([%d%.]+)%s*/%s*([%d%.]+)")
+        local boostLevel = math.min(weapon.chargeLevel, weapon.weaponVisual.iChargeLevels - 1)
+        first = first / chargerBoost ^ boostLevel
+        second = second / chargerBoost ^ boostLevel
+        chargeString = string.format("%.1f / %.1f", first, second)
+    end
+    return Defines.Chain.CONTINUE, chargeString, damageString, shotLimitString
 end)
